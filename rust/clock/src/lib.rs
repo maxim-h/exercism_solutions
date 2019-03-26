781303f6a5e323095d1ccc09b5256aa92c1f0ea7
maxim-h <maksim.holmatov@gmail.com>
2019-03-27 01:14:40 +0300

    started Clock. TODO:rewrite addition

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,23 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock {hours: (hours + minutes/60) % 24,minutes: minutes % 60}
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock {hours:(self.hours + (self.minutes + minutes)/60) % 24, minutes: (self.minutes + minutes) % 60}
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
