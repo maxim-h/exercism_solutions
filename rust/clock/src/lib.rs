574e7b0c69278722e37994c0f17bcf46a59077f9
maxim-h <maksim.holmatov@gmail.com>
2019-05-12 14:03:00 +0300

    cargo fmt

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -3,22 +3,23 @@ use std::fmt;
 #[derive(Debug, PartialEq)]
 pub struct Clock {
     hours: i32,
-    minutes: i32
+    minutes: i32,
 }
 
 fn div_floor(x: i32, divisor: i32) -> i32 {
-    (divisor + x % divisor) % divisor
+    (x as f32 / divisor as f32).floor() as i32
 }
-
 
 fn rem_floor(x: i32, modulus: i32) -> i32 {
-    (x as f32 / modulus as f32).floor() as i32
+    (modulus + x % modulus) % modulus
 }
-
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock {hours: div_floor(hours + rem_floor(minutes, 60), 24), minutes: div_floor(minutes, 60)}
+        Clock {
+            hours: rem_floor(hours + div_floor(minutes, 60), 24),
+            minutes: rem_floor(minutes, 60),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,24 @@
+extern crate iterslide ;
+
+use iterslide::SlideIterator;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+	
+	let mut s: Vec<String> = Vec::with_capacity(list.len());
+	
+	let mut l = list.iter().peekable();
+	
+	match l.peek() {
+		Some(x) => s.push(format!("And all for the want of a {}.", x)),
+		None => return String::from(""),
+	};
+	
+	let l = l.collect::<Vec<&&str>>();
+	
+	for i in l.into_iter().rev().slide(2) {
+		s.push(format!("For want of a {} the {} was lost.", i[1], i[0]))
+	}
+	
+	s.reverse();
+	s.join("\n")
 }
